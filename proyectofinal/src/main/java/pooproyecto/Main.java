42673d0c988be9bc45421dbae0fc71c04aa10856
Carlos Lerdo <carlos4562111@gmail.com>
2023-12-04 10:30:06 -0600

    Proyecto en Github

--- proyectofinal/src/main/java/pooproyecto/Main.java
+++ proyectofinal/src/main/java/pooproyecto/Main.java
@@ -1,7 +1,9 @@
 package pooproyecto;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Hello world!");
+        System.out.println("Prueba");
+        
     }
 }
